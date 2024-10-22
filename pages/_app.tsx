ed06205dc3af6468cc9b6d15cf44dde3b6022253
Henri <henrijjroberts@gmail.com>
2024-10-22 14:15:03 +0100

    Added site title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
 
-export const SITE_TITLE: string = "SITE_TITLE";
+export const SITE_TITLE: string = "Setlist to Playlist";
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
