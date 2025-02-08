357d355e42117f1deddc3e171b317908560da765
H-jj-R <henrijjroberts@gmail.com>
2025-02-08 13:16:30 +0000

    Update i18n configuration to support additional languages; modify HTML document structure

--- lib/constants/i18n.ts
+++ lib/constants/i18n.ts
@@ -2,10 +2,13 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 
+import translationAr from "@locales/ar/translation.json";
+import translationDe from "@locales/de/translation.json";
 import translationEn from "@locales/en/translation.json";
 import translationEs from "@locales/es/translation.json";
 import translationFi from "@locales/fi/translation.json";
 import translationFr from "@locales/fr/translation.json";
+import translationHi from "@locales/hi/translation.json";
 import translationZhCn from "@locales/zh-cn/translation.json";
 
 if (!i18n.isInitialized) {
@@ -13,10 +16,13 @@ if (!i18n.isInitialized) {
         .use(initReactI18next)
         .init({
             resources: {
+                ar: translationAr,
+                de: translationDe,
                 en: translationEn,
                 es: translationEs,
                 fi: translationFi,
                 fr: translationFr,
+                hi: translationHi,
                 "zh-cn": translationZhCn
             },
             fallbackLng: "en",
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
     return (
-        <Html lang="en">
+        <Html>
             <Head>
                 {/* Google AdSense Auto Ads (TODO) */}
                 <script
