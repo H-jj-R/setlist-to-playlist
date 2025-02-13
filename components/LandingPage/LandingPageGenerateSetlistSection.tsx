86f22e9f278e09523a7d7b2c2efb52a40d551e1a
H-jj-R <henrijjroberts@gmail.com>
2025-02-13 15:50:31 +0000

    Changes to LandingPage components styles

--- components/LandingPage/LandingPageGenerateSetlistSection.tsx
+++ components/LandingPage/LandingPageGenerateSetlistSection.tsx
@@ -22,14 +22,14 @@ const GenerateSetlistSection: React.FC = (): JSX.Element => {
             <div id="generate-setlist-btn-container" className="order-last lg:order-first lg:ml-auto lg:mr-auto">
                 <button
                     id="go-to-ai-generate-setlist-btn"
-                    className="mb-10 rounded-full bg-white px-12 py-5 text-lg font-semibold text-purple-600 shadow-lg transition hover:bg-gray-100 focus:outline-none lg:mb-0"
-                    onClick={() => router.push("/ai-generate-setlist")}
+                    className="rounded-full bg-white px-12 py-5 text-lg font-semibold text-purple-600 shadow-lg transition hover:bg-gray-100 focus:outline-none lg:mb-0"
+                    onClick={(): Promise<boolean> => router.push("/ai-generate-setlist")}
                 >
                     {i18n("landingPage:goToAIGenerateSetlist")}
                 </button>
             </div>
             <div id="generate-setlist-text" className="text-center lg:w-1/2 lg:text-right">
-                <h1 id="generate-setlist-title" className="mb-4 text-4xl font-extrabold">
+                <h1 id="generate-setlist-title" className="text-4xl font-extrabold">
                     {i18n("landingPage:generateSetlistTitle")}
                 </h1>
                 <p id="generate-setlist-description" className="text-lg">
