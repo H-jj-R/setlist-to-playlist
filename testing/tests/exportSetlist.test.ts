4c890e8cc814c965e1db4f9fcf562d2a5680eac3
H-jj-R <henrijjroberts@gmail.com>
2025-03-12 20:48:24 +0000

    Add more test cases and increase timeout on some tests

--- testing/tests/exportSetlist.test.ts
+++ testing/tests/exportSetlist.test.ts
@@ -23,23 +23,69 @@ describe("Export Setlist", () => {
         // TODO: Implement test
     }, 10000);
 
+    it("Export dialog on setlist searched by setlist.fm link can be opened", async () => {
+        // TODO: Implement test
+    }, 10000);
+
+    it("Playlist Name field is pre-filled correctly on setlist searched by setlist.fm link", async () => {
+        // TODO: Implement test
+    }, 10000);
+
+    it("Export dialog on setlist searched by setlist.fm link can be closed", async () => {
+        // TODO: Implement test
+    }, 10000);
+
     it("'Combine & Export All' button appears on list of setlists", async () => {
         // TODO: Implement test
     }, 10000);
 
+    it("Export dialog on 'Combine & Export All' can be opened", async () => {
+        // TODO: Implement test
+    }, 10000);
+
+    it("Playlist Name field is pre-filled correctly on 'Combine & Export All'", async () => {
+        // TODO: Implement test
+    }, 10000);
+
+    it("Export dialog on 'Combine & Export All' can be closed", async () => {
+        // TODO: Implement test
+    }, 10000);
+
     it("'Export' button appears on chosen setlist", async () => {
-        // TODO: Implement tests
+        // TODO: Implement test
     }, 10000);
 
     it("Clicking on 'Export' button opens Export dialog", async () => {
-        // TODO: Implement tests
+        // TODO: Implement test
     }, 10000);
 
-    it("Export dialog displays songs from Spotify", async () => {
-        // TODO: Implement tests
+    it("Export dialog displays a list of songs from Spotify", async () => {
+        // TODO: Implement test
     }, 10000);
 
-    // TODO: More test cases
+    it("Playlist Name field is pre-filled correctly for selected setlist", async () => {
+        // TODO: Implement test
+    }, 10000);
+
+    it("Can type in Playlist Name field", async () => {
+        // TODO: Implement test
+    }, 10000);
+
+    it("Can type in Playlist Description field", async () => {
+        // TODO: Implement test
+    }, 10000);
+
+    it("Songs on export list can be selected to be excluded", async () => {
+        // TODO: Implement test
+    }, 10000);
+
+    it("Excluded Songs on export list can be selected again to be included", async () => {
+        // TODO: Implement test
+    }, 10000);
+
+    it("Clicking dropzone opens file chooser", async () => {
+        // TODO: Implement test
+    }, 10000);
 
     afterAll(async () => {
         await delay(100);
